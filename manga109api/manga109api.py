fe9074d9dba88d09705bd82d4d76217e56371b1f
i3ear <ai3ekzk@gmail.com>
2020-08-26 22:29:48 +0900

    add get_annotation method

--- manga109api/manga109api.py
+++ manga109api/manga109api.py
@@ -4,31 +4,34 @@ import xmltodict
 
 
 class Parser(object):
-    def __init__(self, root_dir, book_titles="all"):
+    def __init__(self, root_dir):
         """
         Manga109 annotation parser
 
         Args:
             root_dir (str): The path of the root directory of Manga109 data, e.g., 'YOUR_PATH/Manga109_2017_09_28'
-            book_titles (str or list): The book titles to be parsed.
-                For example, if book_titles = ["ARMS", "AisazuNihaIrarenai"], these two books are read.
-                The default value is "all", where all books are read
         """
         self.root_dir = pathlib.Path(root_dir)
         self.books = []  # book titles
-        self.annotations = {}  # annotation in the form of dict
 
         with (self.root_dir / "books.txt").open("rt", encoding= 'utf-8') as f:
             self.books = [line.rstrip() for line in f]
 
-        for book in self.books:
-            if book_titles != "all" and book not in book_titles:
-                continue
-            with (self.root_dir / "annotations" / (book + ".xml")).open("rt", encoding= 'utf-8') as f:
-                annotation = xmltodict.parse(f.read())
-            annotation = json.loads(json.dumps(annotation))  # OrderedDict -> dict
-            _convert_str_to_int_recursively(annotation)  # str -> int, for some attributes
-            self.annotations[book] = annotation
+    def get_annotation(self, book):
+        """
+        Given a book title, return annotation in the form of dict
+
+        Args:
+            book (str): A title of a book. Should be in self.books.
+
+        Returns:
+            annotation (dict): 
+        """
+        with (self.root_dir / "annotations" / (book + ".xml")).open("rt", encoding= 'utf-8') as f:
+            annotation = xmltodict.parse(f.read())
+        annotation = json.loads(json.dumps(annotation))  # OrderedDict -> dict
+        _convert_str_to_int_recursively(annotation)  # str -> int, for some attributes
+        return annotation
 
     def img_path(self, book, index):
         """
